835b7a73dd73adf0fe37108ca5eca610f86b8bac
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-21 23:05:23 +0530

    Create simple_counter_async_rst.v

---
+++ Use_REG/simple_counter_async_rst.v
@@ -0,0 +1,14 @@
+`timescale 1ns / 1ps
+
+module simple_counter(
+input clk,rst,
+output reg [31:0] count
+    );
+    
+    always @(posedge clk or posedge rst)
+    begin
+    if(rst)
+    count=32'b0;
+    else count=count+1;
+    end
+endmodule
